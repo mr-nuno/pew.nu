3bf252fa8747daad07ac6cb12a6011c9f47bcd80
Peter Wikström <peter@wikstrom.in>
2018-03-20 14:14:33 +0100

    Added api project

---
+++ PEW.Web.Api/Controllers/StatisticsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PEW.Web.Api.Controllers
+{
+    [Produces("application/json")]
+    public class StatisticsController : Controller
+    {
+        [Route("statistics/hockey")]
+        public IActionResult GetHockeyStatistics()
+        {
+            return Ok();
+        }
+    }
+}
